bf8502f0525ff870d66a2503f184d33b1727f65c
Thomas Cannon <tcannon00@gmail.com>
2023-02-05 11:21:20 -0500

    Remove dummy test from gem initialization

--- test/warden/test_webauthn.rb
+++ test/warden/test_webauthn.rb
@@ -6,8 +6,4 @@ class Warden::TestWebAuthn < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Warden::WebAuthn::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
