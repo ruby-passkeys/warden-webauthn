e5886ef6256eeeae36cab3b5a3239c87f0f4a3bd
Thomas Cannon <tcannon00@gmail.com>
2023-02-05 11:39:21 -0500

    Add `WebAuthn::FakeClient` to `WebAuthnTestHelpers`

--- test/test_helper/webauthn_test_helpers.rb
+++ test/test_helper/webauthn_test_helpers.rb
@@ -1,8 +1,14 @@
+require "webauthn/fake_client"
+
 module WebAuthnTestHelpers
   def example_relying_party(options: {})
     return WebAuthn::RelyingParty.new(**{
       origin: "https://example.test",
       name: "Example Relying Party"
     }.merge(options))
+  end
+
+  def fake_client(origin: "https://example.test")
+    return WebAuthn::FakeClient.new(origin)
   end
 end
