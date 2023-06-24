46d27576d84ad5b3d01b1f3d0b5f90f89aa98fd4
Thomas Cannon <tcannon00@gmail.com>
2023-06-23 23:36:34 -0400

    Fix rubocop errors

--- lib/warden/webauthn/rack_helpers.rb
+++ lib/warden/webauthn/rack_helpers.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
+
 module Warden
   module WebAuthn
+    # Helpers that can be mixed in to any Rack middleware or application, in order
+    # to setup the environment for `Warden::WebAuthn`, such as the Relying Party
     module RackHelpers
       def relying_party_key
         "warden.webauthn.relying_party"
